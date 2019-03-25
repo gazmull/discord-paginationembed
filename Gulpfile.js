e368d5e445187487e0b5de9d2144942ac25ba08b
Euni <vescalaw@gmail.com>
2019-03-26 05:59:12 +0800

    Feat: Gulp tasks

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+const { sync: del } = require('del');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tslint = require('gulp-tslint');
+const terser = require('gulp-terser');
+
+const tsProject = ts.createProject('tsconfig.json');
+
+const paths = {
+  bin: 'bin',
+  src: 'src',
+  typings: 'typings',
+};
+
+const tsFiles = {
+  dest: paths.bin,
+};
+
+const terserFiles = {
+  dest: paths.bin,
+  src: paths.bin + '/**/*.js',
+};
+
+gulp.task('clean', () => {
+  del([ paths.bin, paths.typings ]);
+
+  return Promise.resolve(true);
+});
+
+gulp.task('lint', () => {
+  return gulp.src(paths.src)
+    .pipe(tslint({ configuration: './.tslint.js' }))
+    .pipe(tslint.report());
+});
+
+gulp.task('ts', () => {
+  const res = tsProject.src()
+    .pipe(tsProject());
+
+  res.js.pipe(gulp.dest(tsFiles.dest))
+
+  return res.dts.pipe(gulp.dest(paths.typings));
+});
+
+gulp.task('terser', () => {
+  return gulp.src(terserFiles.src)
+    .pipe(terser({ module: true }))
+    .pipe(gulp.dest(terserFiles.dest));
+});
+
+const commonTasks = [ 'lint', 'clean', 'ts' ];
+
+gulp.task('default', gulp.series(...commonTasks, 'terser'));
+gulp.task('build', gulp.series(...commonTasks));
