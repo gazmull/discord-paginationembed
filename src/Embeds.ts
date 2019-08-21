e8d075518d0eb5a8a3382a6fb7e907a6738fe6c0
Euni <vescalaw@gmail.com>
2019-08-21 18:44:08 +0800

    Fix: Message content containing `null` (#4)

--- src/Embeds.ts
+++ src/Embeds.ts
@@ -283,9 +283,9 @@ export class Embeds extends PaginationEmbed<RichEmbed> {
   public async _loadList (callNavigation = true) {
     const shouldIndicate = this.pageIndicator
       ? this.pages === 1
-        ? null
+        ? undefined
         : `Page ${this.page} of ${this.pages}`
-      : null;
+      : undefined;
 
     if (this.clientAssets.message)
       await this.clientAssets.message.edit(shouldIndicate, { embed: this.currentEmbed });
