54a79b040cf11570408e6c0cd78c58e75bb072fa
Kaname <inmortaldragonxspace@outlook.com>
2020-12-28 12:34:16 -0600

    * 🔧 The permission to manage messages is no longer necessary, now what you do first is verify that if you have the permission it will proceed to perform functions such as: delete user reactions, delete promt messages (from the user) and if the bot does not have the permission to manage messages will ignore those functions but the embed will continue to perform the same functions without problems.

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,18 @@
 "use strict";
-
-function e(e) {
-  for (var r in e) exports.hasOwnProperty(r) || (exports[r] = e[r]);
-}
-
-Object.defineProperty(exports, "__esModule", {
-  value: !0
-}), e(require("./Embeds")), e(require("./FieldsEmbed")), e(require("./base")), exports.version = require("../package.json").version;
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.version = void 0;
+__exportStar(require("./Embeds"), exports);
+__exportStar(require("./FieldsEmbed"), exports);
+__exportStar(require("./base"), exports);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+exports.version = require('../package.json').version;
